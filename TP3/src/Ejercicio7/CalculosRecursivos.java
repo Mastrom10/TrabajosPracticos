adea2b78075cb4b599f6ab6274934500755dc104
Nicolas Mastromarino <nicolasmastrom@gmail.com>
2016-05-08 01:47:31 -0300

    Ejercicio 7 recursiones simples COMPLETO.

---
+++ TP3/src/Ejercicio7/CalculosRecursivos.java
@@ -0,0 +1,21 @@
+package Ejercicio7;
+
+/**
+ * Created by Nicolas on 08/05/2016.
+ */
+public class CalculosRecursivos {
+
+    public int calculoFactorial(int numero) {
+        return numero <= 0 ? 1 : numero * calculoFactorial(numero - 1);
+    }
+
+    public int calculoFibonacci(int numero) {
+        if (numero == 0)
+            return 0;
+        else if (numero == 1)
+            return 1;
+        else
+            return calculoFibonacci(numero - 1) + calculoFibonacci(numero - 2);
+    }
+
+}
---
+++ TP3/tests/Ejercicio7/CalculosRecursivosTest.java
@@ -0,0 +1,44 @@
+package Ejercicio7;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nicolas on 08/05/2016.
+ */
+public class CalculosRecursivosTest {
+
+
+    CalculosRecursivos calculador;
+
+    @Before
+    public void setUp() throws Exception {
+        calculador = new CalculosRecursivos();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        calculador = null;
+    }
+
+    @Test
+    public void calculoFactorial() throws Exception {
+        Assert.assertEquals(120, calculador.calculoFactorial(5));
+    }
+
+    @Test
+    public void calculoFibonacci() throws Exception {
+        Assert.assertEquals(0, calculador.calculoFibonacci(0));
+        Assert.assertEquals(1, calculador.calculoFibonacci(1));
+        Assert.assertEquals(1, calculador.calculoFibonacci(2));
+        Assert.assertEquals(2, calculador.calculoFibonacci(3));
+        Assert.assertEquals(3, calculador.calculoFibonacci(4));
+        Assert.assertEquals(5, calculador.calculoFibonacci(5));
+        Assert.assertEquals(8, calculador.calculoFibonacci(6));
+    }
+
+}
